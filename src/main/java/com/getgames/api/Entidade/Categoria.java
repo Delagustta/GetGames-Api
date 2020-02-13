024970961ca52fe38b128d504eae8106c0baaf36
lucas <lucasduarteinacio@hotmail.com>
2020-02-12 22:49:06 -0300

    configuração properties

---
+++ src/main/java/com/getgames/api/Entidade/Categoria.java
@@ -0,0 +1,16 @@
+package com.getgames.api.Entidade;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+
+}
