ae955230eaef87631641588cd1cc9df27dc1966e
Gustavo Funchal <delagusttaf@gmail.com>
2020-02-12 21:10:50 -0300

    Primeiro commit

---
+++ src/main/java/com/getgames/api/GetGamesApiApplication.java
@@ -0,0 +1,16 @@
+package com.getgames.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetGamesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetGamesApiApplication.class, args);
+		
+		
+		System.out.println("Olá mundo!");
+	}
+
+}
---
+++ src/test/java/com/getgames/api/GetGamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getgames.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetGamesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
