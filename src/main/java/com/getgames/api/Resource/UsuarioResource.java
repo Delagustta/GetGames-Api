bb047937773853eb91cf5666b825bd997be0dccb
Gustavo Funchal <delagusttaf@gmail.com>
2020-03-11 00:40:56 -0300

    Ajuste contra bloqueio CORS

--- src/main/java/com/getgames/api/Resource/UsuarioResource.java
+++ src/main/java/com/getgames/api/Resource/UsuarioResource.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.getgames.api.Entity.Usuario;
 import com.getgames.api.Service.UsuarioService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/usuario")
 public class UsuarioResource {
@@ -32,7 +34,7 @@ public class UsuarioResource {
 	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
 		Usuario user = service.salvar(usuario);
 		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
-		
+
 	}
 
 	@PostMapping("/login")
